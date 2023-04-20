a7ee4b5d1f20be626156df29362728564998d808
thiagofqs <thiago.fqs@hotmail.com>
2023-04-20 12:21:16 -0300

    refactor: nome de variáveis modificado.

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,25 @@
 const input = document.getElementById("input-number");
 const btnGuess = document.getElementById("guess");
-btnGuess.addEventListener("click", guess, false);
+btnGuess.addEventListener("click", guess);
 
-const txtNumber = document.getElementById("txt-number");
+const txtNumbersMode = document.getElementById("txt-number");
 const txtResult = document.getElementById("txt-result");
 const txtTip = document.getElementById("txt-tip");
-const txtTry = document.getElementById("txt-try");
+const txtTrys = document.getElementById("txt-try");
 
 const btnEasy = document.getElementById("easy");
 const btnMedium = document.getElementById("medium");
 const btnHard = document.getElementById("hard");
-btnEasy.addEventListener("click", () => {compareDifficulty(btnEasy)}, false);
-btnMedium.addEventListener("click", () => {compareDifficulty(btnMedium)}, false);
-btnHard.addEventListener("click", () => {compareDifficulty(btnHard)}, false);
+btnEasy.addEventListener("click", () => compareDifficulty(btnEasy));
+btnMedium.addEventListener("click", () => compareDifficulty(btnMedium));
+btnHard.addEventListener("click", () => compareDifficulty(btnHard));
 
 const btnTry3 = document.getElementById("try-3");
 const btnTry6 = document.getElementById("try-6");
 const btnTry9 = document.getElementById("try-9");
-btnTry3.addEventListener("click", () => {assignTrys(btnTry3)}, false);
-btnTry6.addEventListener("click", () => {assignTrys(btnTry6)}, false);
-btnTry9.addEventListener("click", () => {assignTrys(btnTry9)}, false);
+btnTry3.addEventListener("click", () => assignTrys(btnTry3));
+btnTry6.addEventListener("click", () => assignTrys(btnTry6));
+btnTry9.addEventListener("click", () => assignTrys(btnTry9));
 
 btnTry3.disabled = true;
 btnEasy.disabled = true;
@@ -45,39 +45,39 @@ function assignMaxNumber() {
     };
 }
 
-function assignTrys(element) {
-    if(gameStarted === false) {
-        const value = parseInt(element.value);
+function assignTrys(el) {
+    if(!gameStarted) {
+        const value = parseInt(el.value, 10);
         if(value === 3) {
             trys = 3;
-            txtTry.innerHTML = "Você tem 3 tentativas";
+            txtTrys.innerHTML = "Você tem 3 tentativas!";
         } else if(value === 6) {
             trys = 6;
-            txtTry.innerHTML = "Você tem 6 tentativas";
+            txtTrys.innerHTML = "Você tem 6 tentativas!";
         } else if(value === 9) {
             trys = 9;
-            txtTry.innerHTML = "Você tem 9 tentativas";
+            txtTrys.innerHTML = "Você tem 9 tentativas!";
         };
-        enableOrDisableButton(element, "try");
+        enableOrDisableButton(el, "try");
     } else {
         txtResult.innerHTML = "Você só pode mudar as tentativas quando perder ou ganhar essa partida!";
     };
 };
 
-function compareDifficulty(element) {
+function compareDifficulty(el) {
     if(gameStarted === false) {
-        const value = parseInt(element.value);
+        const value = parseInt(el.value, 10);
         if(value === 1) { //easy
             difficulty = 1;
-            txtNumber.innerHTML = "Digite um número de 1 a 10";
+            txtNumbersMode.innerHTML = "Digite um número de 1 a 10";
             drawNumber();
         } else if(value === 2) { //medium
             difficulty = 2;
-            txtNumber.innerHTML = "Digite um número de 1 a 50";
+            txtNumbersMode.innerHTML = "Digite um número de 1 a 50";
             drawNumber();
         } else if(value === 3) { //hard
             difficulty = 3;
-            txtNumber.innerHTML = "Digite um número de 1 a 100";
+            txtNumbersMode.innerHTML = "Digite um número de 1 a 100";
             drawNumber();
         };
         enableOrDisableButton(element, "dif");
@@ -86,15 +86,15 @@ function compareDifficulty(element) {
     };
 };
 
-function enableOrDisableButton(element, button) {
-    if(button === "dif") {
+function enableOrDisableButton(el, btn) {
+    if(btn === "dif") {
         elementsAux[0].disabled = false;
-        elementsAux[0] = element;
-    } else if(button === "try") {
+        elementsAux[0] = el;
+    } else if(btn === "try") {
         elementsAux[1].disabled = false;
-        elementsAux[1] = element;
+        elementsAux[1] = el;
     };
-    element.disabled = true;
+    el.disabled = true;
 };
 
 function returnMaxNumberOfDifficulty() {
@@ -108,16 +108,16 @@ function returnMaxNumberOfDifficulty() {
 };
 
 function guess() {
-    let number = input.value;
+    const number = input.value;
     const maxNumber = returnMaxNumberOfDifficulty();
     if(number === "") {
         txtResult.innerHTML = `Você deve digitar um número de 1 a ${maxNumber}.`;
     } else {
         gameStarted = true;
-        number = parseInt(number);
+        number = parseInt(number, 10);
         if(number === secretNumber) {
             input.value = "";
-            txtTry.innerHTML = "Você é um ótimo mentalista!";
+            txtTrys.innerHTML = "Você é um ótimo mentalista!";
             txtResult.innerHTML = `Você acertou o número secreto! O número secreto era ${secretNumber}.`;
             txtTip.innerHTML = "";
             gameStarted = false;
@@ -127,19 +127,19 @@ function guess() {
             trys -= 1;
             if(trys <= 0) {
                 input.value = "";
-                txtTry.innerHTML = `Você tem 0 tentativas!`;
+                txtTrys.innerHTML = `Você tem 0 tentativas!`;
                 txtResult.innerHTML = `Suas tentativas acabaram! O número secreto era ${secretNumber}.`;
                 txtTip.innerHTML = "";
                 gameStarted = false;
             } else {
                 if(number > secretNumber) {
                     input.value = "";
-                    txtTry.innerHTML = `Você tem ${trys} tentativas!`;
+                    txtTrys.innerHTML = `Você tem ${trys} tentativas!`;
                     txtResult.innerHTML = `Você errou! Agora você tem ${trys} tentativas!`;
                     txtTip.innerHTML = `O número secreto é menor que ${number}.`;
                 } else if(number < secretNumber) {
                     input.value = "";
-                    txtTry.innerHTML = `Você tem ${trys} tentativas!`;
+                    txtTrys.innerHTML = `Você tem ${trys} tentativas!`;
                     txtResult.innerHTML = `Você errou! Agora você tem ${trys} tentativas!`;
                     txtTip.innerHTML = `O número secreto é maior que ${number}.`;
                 };
